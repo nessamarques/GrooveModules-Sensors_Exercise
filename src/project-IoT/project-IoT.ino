57bf6d31d5cd898b5ab09c756035fbf59e314251
= <=>
2017-09-12 23:21:00 -0300

    Update project

--- src/project-IoT/project-IoT.ino
+++ src/project-IoT/project-IoT.ino
@@ -12,23 +12,24 @@ const int pinServo = 6;
 const int pinRelay = 5;
 
 float target_temperature = 0;
-float desired_temperature = 30;
-
-//int target_humidity = 0;
-//int water_temperature = 0;
-//int electric_cost = 0;
-//int gas_cost = 0;
-//bool remote_button;
+float desired_temperature = 0;
+float water_temperature = 0;
+float electric_cost = 0;
+float gas_cost = 0;
+float moisture_sensor = 0;
+float moisture_level = 0;
+bool remote_button;
+bool local_button;
 
 char state = 'O';
-
-Servo myservo;
 
 float getTempSensorValue();
 int getHumiSensorValue();
 
 void ServoHandle(int val);
 void RelayHandle(int val);
+
+Servo myservo;
 
 void setup() {
   Serial.begin(9600);
@@ -43,12 +44,10 @@ void loop() {
   delay(100);
   isButtonPressed();
 
-  //parse_command_from_serial();
-  target_temperature = getTempSensorValue();
+  parse_command_from_serial();
   
-  //target_humidity = getHumiSensorValue();
-  //lcd.print("C|Humi:");
-  //lcd.print(humi);
+  target_temperature = getTempSensorValue();
+  moisture_level = getHumiSensorValue();
 
   lcd.clear();
   lcd.print(state);
@@ -61,24 +60,35 @@ void loop() {
   lcd.print(" C ");
 }
 
-
-
 int isButtonPressed(){
   if(digitalRead(pinButton)){
     
     while(digitalRead(pinButton))
-    {
-      Serial.println("Button pressed!");
-    
+    {    
       if(desired_temperature >= target_temperature){
         state = 'O';
+        RelayHandle(1);
       }
       else {
-        // TODO: Validate E x G if button pressed
-        state = 'E';
-  
-        RelayHandle(1);
-        ServoHandle(200);
+        RelayHandle(0);
+
+        if(electric_cost < gas_cost){
+          state = 'E';
+        }
+
+        if(gas_cost < electric_cost){          
+          state = 'G';
+          ServoHandle(200);
+          moisture_sensor = (gas_cost * (1 - (moisture_level/1000) * 8));
+
+          if(moisture_sensor >= 0 && moisture_sensor <= 950)
+          {
+            Serial.println("gas heater efficient");
+          }
+          else{
+            Serial.print("gas heater inefficient");
+          }
+        }
       } 
     }  
   }
@@ -112,35 +122,54 @@ void RelayHandle(int val){
   }
 }
 
-
 /*** PARSE ***/
 #define DEBUG_VAR(X) Serial.print(#X); Serial.print(": "); Serial.println(X);
 
 void apply_command(char command, char value[]) {
+  
   DEBUG_VAR(command);
   switch (tolower(command)) {
     case 'g':
-      float gas_cost = atof(value);
+      gas_cost = atof(value);
       DEBUG_VAR(gas_cost);
     break;
-    // (...)
+    case 'e':
+      electric_cost = atof(value);
+      DEBUG_VAR(electric_cost);
+    break;
+    case 't':
+      desired_temperature = atof(value);
+      DEBUG_VAR(desired_temperature);
+    break;
+    case 'r':
+      remote_button = atof(value);
+      DEBUG_VAR(desired_temperature);
+    break;
+    case 'l':
+      local_button = atof(value);
+      DEBUG_VAR(local_button);
+    break;
+    case 'w':
+      water_temperature = atof(value);
+      DEBUG_VAR(water_temperature);
+    break;
+    case 'm':
+      moisture_level = atof(value);
+      DEBUG_VAR(moisture_level);
+    break;
   }
 }
 
 void parse_command_from_serial() {
-
   while (Serial.available()) {
     char command = Serial.read();
-    
     int index;
     char value[10] = "\0";
-    
+  
     for (index = 0; Serial.available(); index++) {
-      value[index] = Serial.read();
-      
+      value[index] = Serial.read();     
       if (value[index] == '\n' || value[index] == ',') {
-        value[index] = '\0';
-        
+        value[index] = '\0';        
         break;  // command termination character
       }
     }
